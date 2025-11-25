0b8f0d1210edb926f2f1f2a1f08c277db1ed172e
zijie28 <196802554+zijie28@users.noreply.github.com>
2025-11-25 12:23:34 -0500

    little stuff

---
+++ vite-project/src/cards.js
@@ -0,0 +1,5 @@
+export function cards (list , element) {
+    for(i=0; i<element.length; i++) {
+        element.insertAdjacentHTML('beforeend', ``)
+    }
+}
---
+++ vite-project/src/dictionary.js
@@ -0,0 +1,16 @@
+const games = [
+    { name: 'Animal 1', category: 'Animal' },
+    { name: 'Fruit 1', category: 'Fruit' },
+    { name: 'Animal 2', category: 'Animal' },
+    { name: 'Animal 3', category: 'Animal' },
+    { name: 'Fruit 2', category: 'Fruit' },
+    { name: 'Fruit 3', category: 'Fruit' },
+    { name: 'Vehicle 1', category: 'Vehicle' },
+    { name: 'Vehicle 2', category: 'Vehicle' },
+    { name: 'Color 1', category: 'Color' },
+    { name: 'Color 2', category: 'Color' },
+    { name: 'Shape 1', category: 'Shape' },
+    { name: 'Shape 2', category: 'Shape' }
+];
+
+export default games;
