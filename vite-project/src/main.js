29a182774c0d65d87f8453f1f7516967b0b9def5
zijie28 <196802554+zijie28@users.noreply.github.com>
2025-11-26 12:30:46 -0500

    light node dark mode work

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -3,6 +3,18 @@ import { cards } from './cards.js'
 import games from './dictionary.js'
 
 
+document.querySelector('#app').insertAdjacentHTML('beforebegin', `<button id='theme'></button>`)
 
+document.querySelector('#app').insertAdjacentHTML('beforebegin', `<div id='dark'></div>`)
+
+document.querySelector('#theme').addEventListener("click", function(){
+    if(document.getElementById("#light")) {
+        document.getElementById("#light").remove();
+        document.querySelector('#app').insertAdjacentHTML('beforebegin', `<div id='dark'></div>`)
+    } else {
+        document.getElementById("#dark").remove();
+        document.querySelector('#app').insertAdjacentHTML('beforebegin', `<div id='light'></div>`)
+    }
+})
 
 cards(games, document.querySelector('#app'))
