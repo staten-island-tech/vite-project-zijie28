200fae33218e53636e63c2a5f768a17ca6be2112
zijie28 <196802554+zijie28@users.noreply.github.com>
2025-12-02 12:32:24 -0500

    catagory

--- vite-project/src/cards.js
+++ vite-project/src/cards.js
@@ -1,5 +1,5 @@
 export function cards (list, element) {
     for(let i=0; i<list.length; i++) {
-        element.insertAdjacentHTML('beforeend', `<div class="cards"></div>`)
+        element.insertAdjacentHTML('beforeend', `<div class="cards ${list[i].category}" id='${list[i].name}'>${list[i].name}</div>`)
     }
 }
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -2,19 +2,51 @@ import './style.css'
 import { cards } from './cards.js'
 import games from './dictionary.js'
 
+let mode = 'Light'
 
-document.querySelector('#app').insertAdjacentHTML('beforebegin', `<button id='theme'></button>`)
+document.querySelector('.change_theme').insertAdjacentHTML('afterbegin', `<button id='theme'>${mode}</button>`)
 
-document.querySelector('#app').insertAdjacentHTML('beforebegin', `<div id='dark'></div>`)
+
+
+document.querySelector('.category-container').insertAdjacentHTML('afterbegin', `<select id="category" name="category-select">
+    <option value="">All</option>
+    <option value="animal">Animal</option>
+    <option value="fruit">Fruit</option>
+    <option value="vehicle">Vehicle</option>
+    <option value="color">Color</option>
+    <option value="shape">Shape</option>
+</select>`)
+
 
 document.querySelector('#theme').addEventListener("click", function(){
-    if(document.getElementById("#light")) {
-        document.getElementById("#light").remove();
-        document.querySelector('#app').insertAdjacentHTML('beforebegin', `<div id='dark'></div>`)
+    if (document.body.classList.contains("dark")) {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+        mode = 'Dark'
+        document.querySelector('#theme').textContent= mode
     } else {
-        document.getElementById("#dark").remove();
-        document.querySelector('#app').insertAdjacentHTML('beforebegin', `<div id='light'></div>`)
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+        mode = 'Light'
+        document.querySelector('#theme').textContent= mode
     }
+})
+
+document.querySelector('#category').addEventListener('change', function(){
+    if(this.value==='animal'){
+        document.querySelector('#app').innerHTML='';
+    } else if(this.value==='fruit'){
+        document.querySelector('#app').innerHTML='';
+    } else if(this.value==='vehicle'){
+        document.querySelector('#app').innerHTML='';
+    } else if(this.value==='color'){
+        document.querySelector('#app').innerHTML='';
+    } else if(this.value==='shape'){
+        document.querySelector('#app').innerHTML='';
+    } else {
+        cards(games, document.querySelector('#app'))
+    }
+
 })
 
 cards(games, document.querySelector('#app'))
